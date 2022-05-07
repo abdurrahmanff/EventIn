d9a7ed3f725e85c5f7d10a0561e52952dcaf6ef3
abdurrahmanff <abdurrahmanff15@gmail.com>
2022-05-07 17:01:19 +0700

    init commit

---
+++ src/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('contents')
+Homepage
+@endsection
---
+++ src/resources/views/partials/navbar.blade.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-dark" style="background-color: #006FAD">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="/img/foto.jpg" alt="Logo EventIn" width="30" height="24" class="inline-text-top">
+      EventIn
+    </a>
+    <form>
+      <input class="form-control form-control-sm mx-auto" type="search" placeholder="Search" aria-label="Search">
+    </form>
+    <a href="#"">
+        <img src=" /img/foto.jpg" class="rounded-circle" alt="Profil" height="40">
+    </a>
+  </div>
+</nav>
