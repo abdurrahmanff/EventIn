a6b96991a75d74b11e1fedefefbbd37d9de6b2a2
Ahdan <ahdanamanullah@gmail.com>
2022-06-01 15:40:00 +0700

    feat: add layoput for make_event

--- src/resources/views/layouts/main.blade.php
+++ src/resources/views/layouts/main.blade.php
@@ -7,6 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <title>EventIn | {{ $title }}</title>
 </head>
 
@@ -15,6 +16,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
   </script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </body>
 
 </html>
--- src/resources/views/make_event.blade.php
+++ src/resources/views/make_event.blade.php
@@ -6,10 +6,85 @@
   <div class="card start-50 translate-middle-x" style="width: 980px">
     <img src="/img/foto.jpg" class="card-img-top" alt="Upload gambar" height="440px" width="100%">
     <div class="card-body">
-      <form action="">
-        <input type="text" name="" id="" placeholder="Nama Event">
+      <form action="" class="container g-3 needs-validation">
+        <div class="row">
+          <div class="col">
+            <input type="text" class="form-control form-control-lg" name="" style="border:0; border-bottom:1px solid" id="" placeholder="Nama Event" required>
+          </div>
+        </div>
+        <div class="row pt-4">
+          <div class="col">
+            <select class="form-select" id="validationCustom04" required>
+              <option selected disabled value="">Category</option>
+              <option>...</option>
+            </select>
+          </div>
+          <div class="col">
+            <select class="form-select" id="validationCustom04" required>
+              <option selected disabled value="">Category</option>
+              <option>...</option>
+            </select>
+          </div>
+        </div>
+        <div class="row pt-4">
+          <div class="col">
+            <div class="row">
+              <p class="fw-bold">Waktu Event</p>
+              <div class="col">
+                <label for="date" class="col-form-label">Tanggal Mulai</label>
+                <div>
+                    <div class="input-group date">
+                        <input type="date" class="form-control">
+                    </div>
+                </div>
+              </div>
+              <div class="col">
+                <label for="date" class="col-form-label">Tanggal Selesai</label>
+                <div>
+                    <div class="input-group date">
+                        <input type="date" class="form-control">
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="row pt-4">
+              <div class="col">
+                  <label for="time" class="col-form-label">Waktu Mulai</label>
+                    <div class="input-group date">
+                        <input type="time" class="form-control">
+                    </div>
+                </div>
+                <div class="col">
+                  <label for="time" class="col-form-label">Waktu Selesai</label>
+                  <div class="input-group date">
+                      <input type="time" class="form-control">
+                  </div>
+                </div>
+            </div>
+          </div>
+          <div class="col">
+            <p class="fw-bold">Organizer</p>
+            <div class="input-group mb-3">
+              <input type="file" class="form-control" id="inputGroupFile02">
+              <label class="input-group-text" for="inputGroupFile02">Upload</label>
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   </div>
 </div>
+<footer class="p-3" style="background-color: #004E79">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        <div class="fw-bold d-inline-block" style="color: white">Sudah diisi semua? </div>
+        <div class="fw-bold d-sm-inline-block" style="color: #FF7A00">GAS</div>
+      </div>
+      <div class="col-sm text-end">
+        <button type="button" class="btn btn-sm fw-bold" style="background:#FF7A00;color:white;width:100px">GAS</button>
+      </div>
+    </div>
+  </div>
+</footer>
 @endsection
