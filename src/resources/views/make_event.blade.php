cae29da48f2031a0a3b98e96b73ca81f9f010071
abdurrahmanff <abdurrahmanff15@gmail.com>
2022-06-05 16:44:47 +0700

    style: change border and margin

--- src/resources/views/make_event.blade.php
+++ src/resources/views/make_event.blade.php
@@ -2,77 +2,78 @@
 
 @section('contents')
 <div class="container">
-  <h1 class="fw-bold text-center my-3">Buat <span style="color: #FF7A00">Event</span></h1>
-  <div class="card start-50 translate-middle-x" style="width: 980px">
-    <img src="/img/foto.jpg" class="card-img-top" alt="Upload gambar" height="440px" width="100%">
-    <div class="card-body">
-      <form action="" class="container g-3 needs-validation">
-        <div class="row">
-          <div class="col">
-            <input type="text" class="form-control form-control-lg" name="" style="border:0; border-bottom:1px solid" id="" placeholder="Nama Event" required>
-          </div>
-        </div>
-        <div class="row pt-4">
-          <div class="col">
-            <select class="form-select" id="validationCustom04" required>
-              <option selected disabled value="">Category</option>
-              <option>...</option>
-            </select>
-          </div>
-          <div class="col">
-            <select class="form-select" id="validationCustom04" required>
-              <option selected disabled value="">Category</option>
-              <option>...</option>
-            </select>
-          </div>
-        </div>
-        <div class="row pt-4">
-          <div class="col">
-            <div class="row">
-              <p class="fw-bold">Waktu Event</p>
-              <div class="col">
-                <label for="date" class="col-form-label">Tanggal Mulai</label>
-                <div>
-                    <div class="input-group date">
-                        <input type="date" class="form-control">
-                    </div>
-                </div>
-              </div>
-              <div class="col">
-                <label for="date" class="col-form-label">Tanggal Selesai</label>
-                <div>
-                    <div class="input-group date">
-                        <input type="date" class="form-control">
-                    </div>
-                </div>
-              </div>
+  <h3 class="fw-bold text-center">Buat <span style="color: #FF7A00">Event</span></h2>
+    <div class="card start-50 translate-middle-x my-3" style="width: 980px">
+      <img src="/img/foto.jpg" class="card-img-top" alt="Upload gambar" height="440px" width="100%">
+      <div class="card-body">
+        <form action="" class="container g-3 needs-validation">
+          <div class="row">
+            <div class="col">
+              <input type="text" class="form-control form-control-lg rounded-0"
+                style="border:0; border-bottom:1px solid" id="" placeholder="Nama Event" required>
             </div>
-            <div class="row pt-4">
-              <div class="col">
-                  <label for="time" class="col-form-label">Waktu Mulai</label>
+          </div>
+          <div class="row pt-4">
+            <div class="col">
+              <select class="form-select" id="validationCustom04" required>
+                <option selected disabled value="">Category</option>
+                <option>...</option>
+              </select>
+            </div>
+            <div class="col">
+              <select class="form-select" id="validationCustom04" required>
+                <option selected disabled value="">Category</option>
+                <option>...</option>
+              </select>
+            </div>
+          </div>
+          <div class="row pt-4">
+            <div class="col">
+              <div class="row">
+                <p class="fw-bold">Waktu Event</p>
+                <div class="col">
+                  <label for="date" class="col-form-label">Tanggal Mulai</label>
+                  <div>
                     <div class="input-group date">
-                        <input type="time" class="form-control">
+                      <input type="date" class="form-control">
                     </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <label for="date" class="col-form-label">Tanggal Selesai</label>
+                  <div>
+                    <div class="input-group date">
+                      <input type="date" class="form-control">
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row pt-4">
+                <div class="col">
+                  <label for="time" class="col-form-label">Waktu Mulai</label>
+                  <div class="input-group date">
+                    <input type="time" class="form-control">
+                  </div>
                 </div>
                 <div class="col">
                   <label for="time" class="col-form-label">Waktu Selesai</label>
                   <div class="input-group date">
-                      <input type="time" class="form-control">
+                    <input type="time" class="form-control">
                   </div>
                 </div>
+              </div>
+            </div>
+            <div class="col">
+              <p class="fw-bold">Organizer</p>
+              <div class="input-group mb-3">
+                <input type="file" class="form-control" id="inputGroupFile02">
+                <label class="input-group-text" for="inputGroupFile02">Upload</label>
+              </div>
             </div>
           </div>
-          <div class="col">
-            <p class="fw-bold">Organizer</p>
-            <div class="input-group mb-3">
-              <input type="file" class="form-control" id="inputGroupFile02">
-              <label class="input-group-text" for="inputGroupFile02">Upload</label>
-            </div>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
 </div>
 <footer class="p-3" style="background-color: #004E79">
   <div class="container">
