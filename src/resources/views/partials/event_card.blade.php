eac489bd0a9cbde0c83b0c9849447674f59b9014
abdurrahmanff <abdurrahmanff15@gmail.com>
2022-06-08 20:12:31 +0700

    feat: remove 'lupa password'

--- src/resources/views/partials/event_card.blade.php
+++ src/resources/views/partials/event_card.blade.php
@@ -1,13 +1,12 @@
 <div class="col">
   <div class="card" style="width: 16rem">
     <a href="detail-event/{{ $event->id }}" class="stretched-link">
-    @if ($event->img_path)
-      <img src="{{ asset('storage/' . $event->img_path) }}" class="card-img-top" alt="event1"
-        style="height: 9rem">
-    @else
+      @if ($event->img_path)
+      <img src="{{ asset('storage/' . $event->img_path) }}" class="card-img-top" alt="event1" style="height: 9rem">
+      @else
       <img src="https://source.unsplash.com/254x144?{{ $event->category->name }}" class="card-img-top" alt="event1"
         style="height: 9rem">
-    @endif
+      @endif
     </a>
     <div class="card-body py-1">
       <p class="my-0">{{ $event->name }}</p>
