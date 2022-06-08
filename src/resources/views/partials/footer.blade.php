3b53039b872fee03be6637d37e7b317329b0d108
abdurrahmanff <abdurrahmanff15@gmail.com>
2022-06-08 21:43:37 +0700

    style: 100% viewport

--- src/resources/views/partials/footer.blade.php
+++ src/resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer id="footer" class="p-3" style="background-color: #004E79">
-  <div class="fw-bold text-center">Pen buat event?</div>
+  <div class="fw-bold text-center">Ini Footer</div>
   <div class="fw-regular text-break mx-auto" style="width: 497px">Lorem, ipsum dolor sit amet consectetur adipisicing
     elit. Accusantium,
     quibusdam
@@ -7,8 +7,5 @@
     ipsam temporibus delectus placeat aliquid nisi enim eius. Culpa aliquid enim non amet repellat omnis aspernatur
     dicta temporibus sapiente?</div>
   <div class="d-grid col mx-auto mt-3" style="width: 89px">
-    <form action="/buat-event">
-      <button  type="submit" class="btn btn-warning btn-sm fw-bold">Buat Event</button>
-    </form>
   </div>
 </footer>
